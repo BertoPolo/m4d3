ef1c170a544b36ed667309585df447a1f3764442
BertoPolo <bertopolo91@gmail.com>
2022-03-16 14:49:47 +0100

    a

---
+++ d3fabandalberto/src/components/Warning.jsx
@@ -0,0 +1,11 @@
+import { Alert } from "bootstrap"
+
+const Warning = () => {
+  return (
+    <>
+      <Alert variant={"danger"}>This is a alert—check it out!</Alert>
+    </>
+  )
+}
+
+export default Warning
