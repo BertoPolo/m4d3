7da1600acba8e7d818d5c780144961e4dcbf7cdf
BertoPolo <bertopolo91@gmail.com>
2022-03-16 16:56:26 +0100

    a

--- d3fabandalberto/src/components/BookList.jsx
+++ d3fabandalberto/src/components/BookList.jsx
@@ -1,7 +1,31 @@
-const BookList =(data)=>{
-    return(
+// import SingleBook from "./SingleBook"
+import { Component } from "react"
+import { Form } from "react-bootstrap"
 
+class BookList extends Component {
+  state = {
+    searchQuery: "",
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      searchQuery: value,
+    })
+  }
+
+  render() {
+    return (
+      <Form>
+        <Form.Label>Filter</Form.Label>
+        <Form.Control type="text " placeholder="Which book do you want?" />
+      </Form>
     )
+  }
 }
-
 export default BookList
+
+/* data.map((book) => {
+  return <SingleBook data={book} /> 
+   })
+  */
+// const inputFilter data.filter().
